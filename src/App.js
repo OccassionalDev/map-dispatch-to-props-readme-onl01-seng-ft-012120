01c1b7335bb60bca54cf8fdc887cc6d713bee792
Erica Schimmel <ericaschimmel01@gmail.com>
2020-06-11 01:54:28 +0000

    Automatically backed up by Learn

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ class App extends Component {
   }
 
   render() {
-    debugger
+    // debugger
     return (
       <div className="App">
         <button onClick={(event) => this.handleOnClick(event)}>
